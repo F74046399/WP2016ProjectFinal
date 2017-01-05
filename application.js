af0d2ec5e69b3b4cb3d88e1d036834de4000d771
F74046399 <shelly7526@gmail.com>
2017-01-05 20:03:36 +0800

    html & css

--- application.js
+++ application.js
@@ -1,13 +1,9 @@
-$(document).ready(function(){
-  
-  //edit block in the banner
-  function div_show(){
-    document.getFlementById('block1').style.display ="block"; 
-  }
-  function div_hide(){
-    document.getFlementById('block1').style.display ="none"; 
-  }
-  $("#title").on("click",function(){
-      div_show();
- });
-}
+ $(function(){
+ 
+    $('.banner').mouseover(function(){
+    
+    
+    });
+ 
+ }); 
+
